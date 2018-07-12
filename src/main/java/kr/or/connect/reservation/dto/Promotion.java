0d68a6d0064c6e614a13ba30ce7e97a1051f4cd2
LeeNaYoung <iirony@naver.com>
2018-07-13 02:05:33 +0900

    dto 작성

---
+++ src/main/java/kr/or/connect/reservation/dto/Promotion.java
@@ -0,0 +1,23 @@
+package kr.or.connect.reservation.dto;
+
+public class Promotion {
+	private int id;
+	private int product_id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	@Override
+	public String toString() {
+		return "Promotion [id=" + id + ", product_id=" + product_id + "]";
+	}
+	
+}
