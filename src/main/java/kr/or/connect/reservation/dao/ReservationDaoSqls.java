a502c04fda1454c4ddad96efaeeb1f83e6bba8da
LeeNaYoung <iirony@naver.com>
2018-07-15 21:24:10 +0900

    category, product 관련 sql문 작성

---
+++ src/main/java/kr/or/connect/reservation/dao/ReservationDaoSqls.java
@@ -0,0 +1,20 @@
+package kr.or.connect.reservation.dao;
+
+public class ReservationDaoSqls {
+	//category
+	public static final String CATEGORY_ALL="select id, name from category";
+	
+	//product
+	public static final String ALL_COUNT="select count(*) from product";
+	public static final String CATE_COUNT="select count(*) from product where category_id=:category_id";
+	public static final String PRODUCT_ALL="select id, category_id, description, content, event, create_date, modify_date from product";
+	public static final String PAGING="select content, description from product where category_id= :category_id order by id desc limit :start, :limit";
+	
+	//promotion
+	public static final String PROMOTION_ALL="select id, product_id from promotion";
+	
+	//display_info
+	public static final String PLACE="select place_name from display_info where product_id=:product_id";
+	
+
+}
